d10ed32e840c04869ce9d2debe504bbd3db66c5a
sillyskull <rohan5144320@gmail.com>
2024-11-10 04:15:30 +0530

    second commit

---
+++ api/models/image.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const imageSchema = mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        refs:"users"
+    },
+    uploaded_image_path: {
+        type: String,
+        required: true
+    }
+}, {timestamps:true});
+
+const Image = mongoose.model('image', imageSchema);
+
+module.exports = Image;
