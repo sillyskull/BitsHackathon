6d8cf4334a507aee78d7e27006560b57ff157e4c
Ritesh2006M <riteshreddy65@gmail.com>
2024-11-10 04:06:36 +0530

    "Login page fixed"

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter, createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import Login from "./pages/Login/Login";
+import Login from './components/login';
 import Dashboard from './components/dashboard/index';
 import Signup from './components/signup';
 import Home from './components/home';
@@ -33,7 +33,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
